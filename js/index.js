365c4e4da91c4f896c645249f52dfb831487dee3
Rafael Hengles <rhengles@gmail.com>
2020-07-03 09:34:46 -0300

    Clean js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,4 @@
 
-var users = Vue.readonly([
-	{ name: 'James' },
-	{ name: 'Jessica' },
-	{ name: 'John' }
-]);
-
-function showUserModal(id) {
-	var bgView = router.currentRoute.value.fullPath;
-	return router.push({
-		name: 'user',
-		params: { id: String(id) },
-		state: { bgView: bgView }
-	});
-}
-
-function closeUserModal() {
-	history.back();
-}
-
 var AppComp = {};
 
 var appLoader = prefixMatcher({
@@ -43,49 +24,3 @@ appLoader('app--root')()
 .catch(function(err, load) {
 	console.error('Error loading root component', err, load);
 });
-
-/*
-var _shift$ = Vue.createApp({
-	setup: function() {
-		var state = Vue.reactive({
-			value: 42
-		});
-		return {
-			state: state
-		};
-	},
-	getComponent: function(spellings, instance) {
-		// example of spellings object:
-		// {
-		// 	"raw": "shift-foo",
-		// 	"hyphenated": "shift-foo",
-		// 	"camelized": "shiftFoo",
-		// 	"PascalCase": "ShiftFoo"
-		// }
-		var prom = appLoader(spellings.hyphenated);
-		if ( prom ) {
-			console.log('got loader', spellings, prom, instance);
-			prom.then(function(comp, load) {
-				console.log('got loader success', comp, load);
-			});
-			prom.catch(function(error, load) {
-				console.error('got loader failure', error, load);
-			});
-			return Vue.defineAsyncComponent(function() { return prom; });
-		} else {
-			console.log('App GetComponent', spellings, instance);
-		}
-		return {
-			template: '<div><p>She\'s like the {{state.shesLikeThe}}</p></div>',
-			setup: function() {
-				var state = Vue.reactive({
-					shesLikeThe: 'wind'
-				});
-				return {
-					state: state
-				};
-			}
-		}
-	}
-}).mount('#root');
-*/
